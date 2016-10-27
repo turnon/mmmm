ede666b42618a11460bcf2be2a66f445612c2717
ken <block24block@gmail.com>
2016-10-27 15:45:09 +0800

    open-class defined methods come first

--- lib/mmmm/object_methods.rb
+++ lib/mmmm/object_methods.rb
@@ -3,41 +3,54 @@ module Mmmm
 
     class FileLineMethod
       attr_reader :file, :line, :method
-      def initialize src_loc, m
+      def initialize src_loc, m, inherited
         @file = src_loc[0]
         @line = src_loc[1]
         @method = m
+	@inherited = inherited
+      end
+
+      def inherited?
+        @inherited
       end
     end
 
     def initialize obj
-      file = nil
-      flm_arr = file_line_method_arr(obj)
-      @group_by_file = group_by_file(flm_arr)
+      @obj = obj
     end
 
     def inspect
-      @group_by_file.
-	map {|file, flms| "#{file.cyan}\n#{lm_in_string(flms)}" }.
+      group_by_inherited_and_file.
+	map {|inh_file, flms| "#{inh_file[1].cyan} #{inh_file[0]}\n#{lm_in_string(flms)}" }.
 	join("\n")
     end
 
     private
 
-    def file_line_method_arr obj
-      obj.methods.reduce([]) do |rs, m|
-        file_line = obj.method(m).source_location
+    def file_line_method_arr
+      @obj.methods.each_with_object([]) do |m, rs|
+        file_line = @obj.method(m).source_location
         if file_line
-          rs << FileLineMethod.new(file_line, m)
+          rs << FileLineMethod.new(file_line, m, inherited?(m))
         end
-        rs
       end
     end
 
-    def group_by_file flm_arr
-      grouped = flm_arr.
-	group_by{|flm| flm.file}.
-	sort_by{|file, flm| file}
+    def inherited? method
+      not open_class_defined_methods.include? method
+    end
+
+    def open_class_defined_methods
+      @ocdm ||= [:singleton_class, :class].
+	map{|klass| @obj.send(klass).instance_methods(false)}.
+	flatten.
+	uniq
+    end
+
+    def group_by_inherited_and_file
+      grouped = file_line_method_arr.
+	group_by{|flm| [flm.inherited?.to_s, flm.file]}.
+	sort_by{|inh_f, flm| inh_f}
 
       Hash[grouped]
     end
